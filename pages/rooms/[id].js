d304555653921174dc07381893383965ee39bfb1
khadetou <khadetou96@gmail.com>
2021-07-24 21:16:19 +0000

    room feature

--- pages/rooms/[id].js
+++ pages/rooms/[id].js
@@ -58,12 +58,12 @@ export default function RoomDetails() {
             <h3 className="mb-4">Features:</h3>
             <div className="room-feature">
               <i className="fa fa-cog fa-fw fa-users" aria-hidden="true"></i>
-              <p>{guestCapacity}</p>
+              <p>{guestCapacity} Guests</p>
             </div>
 
             <div className="room-feature">
               <i className="fa fa-cog fa-fw fa-bed" aria-hidden="true"></i>
-              <p>{numOfBeds}</p>
+              <p>{numOfBeds} Beds</p>
             </div>
 
             <div className="room-feature">
@@ -72,8 +72,63 @@ export default function RoomDetails() {
             </div>
 
             <div className="room-feature">
-              <i className="fa fa-cog fa-fw fa-cutlery" aria-hidden="true"></i>
-              <p>Kitchen</p>
+              <i
+                className={
+                  breakfast
+                    ? "fa fa-check text-success"
+                    : "fa fa-times text-danger"
+                }
+                aria-hidden="true"
+              ></i>
+              <p>Breakfast</p>
+            </div>
+
+            <div className="room-feature">
+              <i
+                className={
+                  internet
+                    ? "fa fa-check text-success"
+                    : "fa fa-times text-danger"
+                }
+                aria-hidden="true"
+              ></i>
+              <p>Internet</p>
+            </div>
+
+            <div className="room-feature">
+              <i
+                className={
+                  airConditioned
+                    ? "fa fa-check text-success"
+                    : "fa fa-times text-danger"
+                }
+                aria-hidden="true"
+              ></i>
+              <p>Air Conditioned</p>
+            </div>
+
+            <div className="room-feature">
+              <i
+                className={
+                  petsAllowed
+                    ? "fa fa-check text-success"
+                    : "fa fa-times text-danger"
+                }
+                aria-hidden="true"
+              ></i>
+              <p>Pets Allowed</p>
+            </div>
+
+            <div className="room-feature">
+              <i
+                className={
+                  roomCleaning
+                    ? "fa fa-check text-success"
+                    : "fa fa-times text-danger"
+                }
+                aria-hidden="true"
+              ></i>
+              <p>Room Cleaning</p>
             </div>
           </div>
         </div>
@@ -81,7 +136,7 @@ export default function RoomDetails() {
         <div className="col-12 col-md-6 col-lg-4">
           <div className="booking-card shadow-lg p-4">
             <p className="price-per-night">
-              <b>{pricePerNight}$</b> / night
+              <b>${pricePerNight}</b> / night
             </p>
 
             <button className="btn btn-block py-3 booking-btn">Pay</button>
