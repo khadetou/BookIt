19119c80a39c89fb3f42164a938904acefd446f1
khadetou <khadetou96@gmail.com>
2021-07-29 10:24:19 +0000

    show user in the header

--- components/Header.js
+++ components/Header.js
@@ -28,11 +28,48 @@ const Header = () => {
         </div>
 
         <div className="col-3 mt-3 mt-md-0 text-center">
-          <Link href="/login">
-            <a className="btn btn-danger px-4 text-white login-header-btn float-right">
-              Login
-            </a>
-          </Link>
+          {user ? (
+            <div className="ml-4 dropdown d-inline">
+              <a
+                className="btn dropdown-toggle ml-4"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <figure className="avatar avatar-nav">
+                  <img
+                    src={user.avatar && user.avatar.url}
+                    alt={user && user.name}
+                    className="rounded-circle"
+                  />
+                </figure>
+                <span>{user && user.name}</span>
+              </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton"
+              >
+                <Link href="/bookings/me">
+                  <a className="dropdown-item">My Bookings</a>
+                </Link>
+                <Link href="/me/Update">
+                  <a className="dropdown-item">Profile</a>
+                </Link>
+                <Link href="/">
+                  <a className="dropdown-item">Log out</a>
+                </Link>
+              </div>
+            </div>
+          ) : (
+            !loading && (
+              <Link href="/login">
+                <a className="btn btn-danger px-4 text-white login-header-btn float-right">
+                  Login
+                </a>
+              </Link>
+            )
+          )}
         </div>
       </div>
     </nav>
