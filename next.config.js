36174a1f187f297f12153c3959366c7715e153d9
khadetou <khadetou96@gmail.com>
2021-08-16 13:52:13 +0000

    stripe api key and stripe secret key

--- next.config.js
+++ next.config.js
@@ -12,6 +12,10 @@ module.exports = {
     SMTP_PASS: "56e97bf6f83a15",
     SMTP_FROM_NAME: "bookIt",
     SMTP_FROM_EMAIL: "noreply@bookit.com",
+    STRIPE_API_KEY:
+      "pk_test_51JP61kKdYfcVMwHw2V3wS32oFPZZxINq3Eu2ZPjrWnqB2X0oecvc59sL31wL55kGayGtJfM9SbXC1m544JNCbvY000l2SQetOf",
+    STRIPE_SECRET_KEY:
+      "sk_test_51JP61kKdYfcVMwHwADAgEIbKKqdzGmju6tkkUqEzZXtO9QT7thcDa516q9qGNzp3zkwqN24yIFWSoSDYpLLKKaCx00F4tXvTR6",
   },
   images: {
     domains: ["res.cloudinary.com"],
