f8bc1cd132c165fdb3cb83d75523f0eacd14e6d4
khadetou <khadetou96@gmail.com>
2021-12-16 11:08:29 +0000

    body exeeded 1mb fixed

--- pages/api/me.js
+++ pages/api/me.js
@@ -7,6 +7,14 @@ const handler = nc({ onError });
 
 connectDB();
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "5mb",
+    },
+  },
+};
+
 handler.use(isAuthenticated).get(getUser);
 handler.use(isAuthenticated).put(updateProfile);
 
